27889af3471b757210534fe13dd945b9e9f706af
PokornýStanislav <PokornStanislav@V315>
2026-03-10 09:24:15 +0100

    test tlacitka

--- prjjj/Form1.cs
+++ prjjj/Form1.cs
@@ -6,5 +6,10 @@ namespace prjjj
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
